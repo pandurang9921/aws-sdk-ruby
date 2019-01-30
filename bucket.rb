d0f64244edaae17b1574c965ab6f6f03e4dae9db
pandurang patil <pandurangpatil746@gmail.com>
2019-01-30 23:34:22 +0530

    this is my second commit

--- bucket.rb
+++ bucket.rb
@@ -3,13 +3,14 @@ require 'json'
 
 profile_name = 'pandurang'
 region = "us-east-1"
-bucket = 'doc-sample-bucket'
-my_bucket = 'david-cloud'
+bucket = 'doc-sample-ruby'
+my_bucket = 'cloud12'
 
 # S3
 
 # Configure SDK
 s3 = Aws::S3::Client.new(profile: profile_name, region: region)
+s3.create_bucket(bucket: bucket)
 resp = s3.list_buckets
 resp.buckets.each do |bucket|
   puts bucket.name
